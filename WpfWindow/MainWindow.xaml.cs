dbd49e2767accdb2ca8e921992668342b75d3311
Semen Teplov <eburg-severca@mail.ru>
2023-05-09 22:23:16 +0500

    Add files via upload

---
+++ WpfWindow/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+namespace WpfWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives()) 
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Tag = drive;
+                item.Header = drive.ToString();
+                item.Items.Add("*");
+                tView.Items.Add(item);
+            }
+        }
+
+        private void tView_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)e.OriginalSource;
+            item.Items.Clear();
+            DirectoryInfo dir;
+
+            if (item.Tag is DriveInfo)
+            {
+                DriveInfo drive = (DriveInfo)item.Tag;
+                dir = drive.RootDirectory;
+            }
+            else
+            {
+                dir = (DirectoryInfo)item.Tag;
+            }
+
+            try
+            {
+                foreach (DirectoryInfo sDir in dir.GetDirectories())
+                {
+                    TreeViewItem newItem = new TreeViewItem();
+                    newItem.Tag = sDir;
+                    newItem.Header = sDir.ToString();
+                    newItem.Items.Add("*");
+                    item.Items.Add(newItem);
+                }
+            }
+            catch
+            {
+                { }
+            }
+        }
+    }
+}
